80a710b534cc55e4489bd38bd505184586493dc2
Serge Fomin <serge.fo@gmail.com>
2016-05-18 04:12:11 -0700

    Added standard php modules

--- Dockerfile.php
+++ Dockerfile.php
@@ -5,6 +5,27 @@ MAINTAINER Serge Fomin <serge.fo@gmail.com>
 RUN apk update \
   && apk add \
     php-fpm \
+    php-json \
+    php-zlib \
+    php-xml \
+    php-pdo \
+    php-phar \
+    php-openssl \
+    php-pdo_mysql \
+    php-mysqli \
+    php-gd \
+    php-iconv \
+    php-mcrypt \
+    php-mysql \
+    php-curl \
+    php-opcache \
+    php-ctype \
+    php-apcu \
+    php-intl \
+    php-bcmath \
+    php-dom \
+    php-xmlreader \
+    mysql-client \
   && rm -rf /tmp/* /var/cache/apk/*
 
 COPY etc/php/php-fpm.conf /etc/php/php-fpm.conf
@@ -14,4 +35,5 @@ RUN mkdir /app
 
 CMD php-fpm \
   -c /etc/php/php-fpm.conf \
+  --nodaemonize \
   --allow-to-run-as-root
